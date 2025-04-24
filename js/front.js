31ad360e4ae864ef649809ac312088a4fb48b763
Eduardo Fabiani <128606132+EduardoHenriqueFabini@users.noreply.github.com>
2025-04-24 15:53:16 -0300

    Add files via upload

---
+++ js/front.js
@@ -0,0 +1,20 @@
+function mostrarSenha(){
+    var senha = document.getElementById('senha');
+    var mostrarSenha = document.getElementById('mostrar_senha');
+
+    if (senha.type === 'password'){
+        senha.setAttribute('type', 'text')
+        mostrarSenha.classList.replace('bi-eye-slash','bi-eye')
+    }
+    else{
+        senha.setAttribute('type', 'password')
+        mostrarSenha.classList.replace('bi-eye','bi-eye-slash')
+    }
+}
+ function mostrarData(){
+
+ }
+
+ function esconderData(){
+
+ }
